fb0c3c123c94027c1b5c4e73c9297abc24754726
ymrlk <moryliak.y@gmail.com>
2023-06-03 16:35:49 +0300

    inject-bootstrap  - install bootstrap 5  - provide default routes for login, register, feed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component')
+      .then((cmp) => cmp.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.component')
+      .then((cmp) => cmp.RegisterComponent)
+  },
+  {
+    path: 'feed',
+    loadComponent: () => import('./feed/feed.component')
+      .then((cmp) => cmp.FeedComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+];
