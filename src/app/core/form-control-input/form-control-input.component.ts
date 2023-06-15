c4676655e77491352b19b1c4b391a11a7092de64
ymrlk <moryliak.y@gmail.com>
2023-06-15 20:38:04 +0300

    Edit-profile   - remove console.log

--- src/app/core/form-control-input/form-control-input.component.ts
+++ src/app/core/form-control-input/form-control-input.component.ts
@@ -43,7 +43,6 @@ export class FormControlInputComponent implements ControlValueAccessor {
   }
 
   writeValue(outsideValue: string): void {
-    console.log(outsideValue);
     this.value = outsideValue;
   }
 
